68af4764a52389732a1f6d2859f45229907819aa
Ethan Martin <60288171+yuckdevchan@users.noreply.github.com>
2023-10-17 23:19:16 +0100

    Add mouse sound button script

---
+++ mouse.py
@@ -0,0 +1,27 @@
+import subprocess
+import random
+from pynput.mouse import Listener
+
+sounds = ["bruh", "obamna", "sus", "wah", "fazbear"]
+sound_queue = sounds.copy()
+random.shuffle(sound_queue)
+
+def play_random_sound():
+    if sound_queue:
+        sound = sound_queue.pop(0)
+        subprocess.run(["echo", f"Playing: {sound}.mp3"])
+        subprocess.run(["echo", f"{sound}.mp3"])
+    else:
+        random.shuffle(sounds)
+        sound_queue.extend(sounds)
+
+def on_click(x, y, button, pressed):
+    if pressed:
+        play_random_sound()
+
+def main():
+    with Listener(on_click=on_click) as listener:
+        listener.join()
+
+if __name__ == "__main__":
+    main()
