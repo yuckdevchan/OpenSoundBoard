fdd9df7123ab842811766220eefbb1fd78bd3a26
Ethan Martin <60288171+yuckdevchan@users.noreply.github.com>
2023-10-18 20:51:06 +0100

    stuff

--- mouse.py
+++ mouse.py
@@ -3,21 +3,27 @@ import random
 from pynput.mouse import Listener
 
 sounds = [
-    "bruh", 
-    "obamna", 
-    "sus", 
-    "wah", 
-    "fazbear"
+    "bruh",
+    "obamna",
+    "sus",
+    "wah",
+    "fazbear",
+    "bababooey"
 ]
 
 sound_queue = sounds.copy()
 random.shuffle(sound_queue)
+current_sound_process = None
 
 def play_random_sound():
+    global current_sound_process
+    if current_sound_process:
+        current_sound_process.kill()
+
     if sound_queue:
         sound = sound_queue.pop(0)
-        subprocess.run(["echo", f"Playing: {sound}.mp3"])
-        subprocess.run(["mplayer", f"{sound}.mp3"])
+        print(f"Playing: {sound}.mp3")
+        current_sound_process = subprocess.Popen(["mplayer", f"{sound}.mp3"])
     else:
         random.shuffle(sounds)
         sound_queue.extend(sounds)
